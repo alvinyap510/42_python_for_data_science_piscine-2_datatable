9b4618dfc7a007e62dd88c9101ff59bd05bb4abd
Alvin Yap <leaf.alvin@gmail.com>
2023-10-03 14:17:30 +0800

    fix: ex01 xticks and scale

--- ex01/aff_file.py
+++ ex01/aff_file.py
@@ -18,11 +18,17 @@ def main():
         # Sets the country column as the index of malaysia_data
         malaysia_data.set_index('country', inplace=True)
 
+        start_index = 0  # Index corresponding to the year 1800
+        end_index = 251  # Index corresponding to the year 2050
+
         # return the column names of malaysia_data as panda index
         years = malaysia_data.columns.astype(int).to_numpy()
+        years = years[start_index:end_index]
 
         # Select the row corresponding Malaysia
         life = malaysia_data.loc['Malaysia'].to_numpy()
+        life = life[start_index:end_index]
+
 
         # Initialize a plot
         fig, axs = plt.subplots()
@@ -33,6 +39,8 @@ def main():
         # Plot Years as x-axis and Life as y-axis
         axs.plot(years, life)
         axs.set_xlabel("Years")
+        axs.set_xticks([1800, 1840, 1880, 1920, 1960, 2000, 2040])
+        axs.set_xticklabels(['1800', '1840', '1880', '1920', '1960', '2000', '2040'])
         axs.set_ylabel("Life Expectancy")
 
         plt.show()
